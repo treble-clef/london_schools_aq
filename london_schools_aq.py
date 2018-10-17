4dcb17d1d803c61ff81dbf4b65882c7d51f81032
May B <43228840+treble-clef@users.noreply.github.com>
2018-10-17 13:13:43 -0700

    Update london_schools_aq.py

--- london_schools_aq.py
+++ london_schools_aq.py
@@ -2,15 +2,17 @@
 ## Interactive Data Analysis ##
 ###############################
 ## The london_schools_aq module allows for the user to interactively select a specific city or school type to get the average NO2 level.
+## User please remember to change the file location in line 5 to your own.
 
 def london_schools_aq():
   import numpy as np
   import pandas as pd
+  ## Change file location to yours:
   file = "C:/Users/mayda/Downloads/Schools_ParliamentaryConstituencies.csv"
   df = pd.read_csv(file)
   startpage = {"A":" NO2 Levels by City", "B":" NO2 Levels by School Type"}
   cities = {"a":"Barking and Dagenham", "b":"Barnet", "c":"Bexley", "d":"Brent", "e":"Bromley", 
-  "f":"Camden", "g":"Croydon", "h":"Ealing", "i":"Enfield", "j":"Greenwich", "k":"Hackney", 
+  "f":"Camden", "g":"Croydon", "h":"Ealing", "i":"Enfield", "j":"Greenwich", "k":"Hackney",     
   "l":"Hammersmith and Fulham", "m":"Haringey", "n":"Harrow", "o":"Havering", "p":"Hillingdon", 
   "q":"Hounslow", "r":"Islington", "s":"Kensington and Chelsea", "t":"Kingston upon Thames", 
   "u":"Lambeth", "v":"Lewisham", "w":"City of London", "x":"Merton", "y":"Newham", "z":"Redbridge", 
@@ -26,7 +28,7 @@ def london_schools_aq():
   "u":"Other Independent School", "v":"Other Independent Special School", "w":"Pupil Referral Unit",
   "x":"Sixth Form Centres", "y":"Special Post 16 Institution", "z":"Studio Schools", 
   "aa":"University Technical College", "bb":"Voluntary Aided School", "cc":"Voluntary Controlled School",
-  "dd":"Miscellaneous"}
+  "dd":"Miscellaneous"} 
   print("")
   print("    WELCOME TO THE LONDON SCHOOLS ENVIRONMENTAL AIR QUALITY DATASET.")
   print("")
@@ -34,7 +36,7 @@ def london_schools_aq():
   for key in startpage:
     print(key, ":", startpage[key])
   print("")
-  inputoption1 = input("What do you want to analyze? (case sensitive) ")
+  inputoption1 = input("What do you want to analyze? (enter letter choice, case sensitive) ")
   print("")
   for key in startpage:
     if inputoption1 in startpage:
@@ -44,17 +46,15 @@ def london_schools_aq():
       print("You have chosen an invalid option. Please select again.")
       print("")
       inputoption1 = input("What do you want to analyze? ")
-  if inputoption1 == "A":
+  if inputoption1 == "A": 
     print("")
     print("The following cities are available:")
     input("(press Enter to view cities...)")
     for key in cities:
       print(key, ":", cities[key])
-    inputoption2 = input("Select a city to analyze (case-sensitive letter):")
+    inputoption2 = input("Select a city to analyze (enter letter choice, case-sensitive letter):")
     for key in cities:
       if inputoption2 in cities:
-        print("")
-        print("You have chosen: %s.%s" %(inputoption2, cities[inputoption2]))
         print("")
         inputcity = df["Local Authority name"] == cities[inputoption2]
         df2 = df[inputcity]
@@ -66,6 +66,7 @@ def london_schools_aq():
         print("")
         inputoption2 = input("Select a city to analyze (case-sensitive letter): ")
   elif inputoption1 == "B":
+    print("")
     print("The following school types are available:")
     input("(press Enter to view school types...)")
     for key in schooltypes:
@@ -74,7 +75,6 @@ def london_schools_aq():
     for key in schooltypes:
       if inputoption3 in schooltypes:
         print("")
-        print("You have chosen: %s" %inputoption3)
         inputschool = df["Type of Establishment"] == schooltypes[inputoption3]
         df3 = df[inputschool]
         schoolavg = df3["NO2ug/m3 mean 2013"].mean()
